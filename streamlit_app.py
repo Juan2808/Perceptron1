357e98d8c8ed8b730ecc55f5de648caf19f21f1e
Juan2808 <juan.salima@correo.unimet.edu.ve>
2025-04-29 16:11:34 -0400

    actualizacion2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,13 @@ import streamlit as st
 st.title('🎈 App Name')
 
 st.write('Haiendo un ligero cambio world!')
+
+
+st.write('Haiendo un ligero cambio world!')
+
+
+# Vamos a hacer el perceptron 
+
+num1 = st.number_input("Introduce el primer número", value=0)
+num2 = st.number_input("Introduce el segundo número", value=0)
+ 
